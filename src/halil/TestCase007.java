044761aa6a7d91f4a6da5b51412317d382fdb42c
halilozturk <ztrkhll58@gmail.com>
2022-06-18 13:20:40 +0200

    3rd commit of tc006 branch

--- src/halil/TestCase007.java
+++ src/halil/TestCase007.java
@@ -5,5 +5,7 @@ public class TestCase007 {
     public static void main(String[] args) {
 
         System.out.println("2nd commit in tc006 branch");
+        System.out.println("\"---------      \" = " + "---------      ");
+        System.out.println("3rd commit in tc006 branch");
     }
 }
